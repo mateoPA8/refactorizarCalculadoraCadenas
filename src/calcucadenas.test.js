e1169c61cfe5d903c03ea3bf9657feca09ee6aab
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-05-14 00:18:00 -0400

    devuelve el unico valor que lleva la cadena

--- src/calcucadenas.test.js
+++ src/calcucadenas.test.js
@@ -4,4 +4,7 @@ describe("Sumar", () => {
   it("deberia devolver 0 si se envia una cadena sin caracteres", () => {
     expect(calculadora("")).toEqual(0);
   });
+  it("deberia devolver el unico valor que lleva la cadena", () => {
+    expect(calculadora("1")).toEqual(1);
+  });
 });
--- src/calcucandenas.js
+++ src/calcucandenas.js
@@ -1,6 +1,9 @@
 function calculadora(chain) {
   if(chain === "")
     return 0;
+  else{
+    return Number(chain);
+  }
 }
 
 export default calculadora;
