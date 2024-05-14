8ac9aafa56195905268d211050e56ef9866c2a39
Adrian Augusto Rojas Ortuño <adrian.rojas.o@ucb.edu.bo>
2024-05-14 00:52:20 -0400

    Refactoring: Se cambio el codigo de produccion para ser mas legible

--- src/calcucandenas.js
+++ src/calcucandenas.js
@@ -1,18 +1,20 @@
 function calculadora(chain) {
+  let numbers = chain.split(/[,|-]/);
+  let acc = 0;
+
   if(chain === "")
     return 0;
-  else{
-    let numbers = chain.split(/[,|-]/);
-    let acc = 0;
-    if(numbers.length == 1)
-      return Number(numbers[0]);
-    else
-      for(const num of numbers){
-        if(num <= 1000)
-          acc = acc + Number(num);
-      }    
-    return acc;
-  }
+
+  if(numbers.length == 1)
+    return Number(numbers[0]);
+
+  else
+    for(const num of numbers){
+      if(num <= 1000)
+        acc = acc + Number(num);
+    }    
+
+  return acc;
 }
 
 export default calculadora;
